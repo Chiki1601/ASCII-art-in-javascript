efa2eb4cafa22c9c3af6ab971b8a320415c221a1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 11:13:13 +0530

    Create gm1.js

---
+++ Good Morning/gm1.js
@@ -0,0 +1,20 @@
+
+// good morning
+     
+console.log("🌷🌹🍃🌹🌷G\n");
+console.log("┏╮╭┓┏╮╭┓ O\n");
+console.log("╰╭╮╯╰╭╮╯ O\n");
+console.log("╭╰╯╮╭╰╯╮ D\n");
+console.log("┗╯╰┛┗╯╰┛ \n");
+console.log("🌷🌹🍃🌹🌷 M\n");
+console.log("┏╮╭┓┏╮╭┓  O\n");
+console.log("╰╭╮╯╰╭╮╯  R\n");
+console.log("╭╰╯╮╭╰╯╮  N\n");
+console.log("┗╯╰┛┗╯╰┛   I\n");
+console.log("🌷🌹🍃🌹🌷 N\n");
+console.log("┏╮╭┓┏╮╭┓  G\n");
+console.log("╰╭╮╯╰╭╮╯  \n");
+console.log("╭╰╯╮╭╰╯╮  D\n");
+console.log("┗╯╰┛┗╯╰┛  E\n");
+console.log("🌷🌹🍃🌹🌷 A\n");
+console.log("      FRIEND     \n");
